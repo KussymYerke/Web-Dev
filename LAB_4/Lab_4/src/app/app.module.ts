ac481dc4c6723d36fb451dedb5075a403b6a4759
Yerkebulan Kussym <e_kusym@kbtu.kz>
2023-03-01 12:40:52 +0600

    umpalumpa

--- LAB_4/Lab_4/src/app/app.module.ts
+++ LAB_4/Lab_4/src/app/app.module.ts
@@ -1,31 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { WatchesComponent } from './watches/watches.component';
+import { LaptopsComponent } from './laptops/laptops.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
+      {path: '', component: ProductListComponent },
+      {path: 'watches', component: WatchesComponent},
+      {path: 'laptop', component: LaptopsComponent},
+      {path: '**', component: NotFoundComponent}
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    WatchesComponent,
+    LaptopsComponent,
+    NotFoundComponent
   ],
   bootstrap: [
     AppComponent
   ]
 })
+
+
 export class AppModule { }
 
 
