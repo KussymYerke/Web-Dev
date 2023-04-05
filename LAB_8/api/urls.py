1591fa2c0b436a306cd780c7070a0c5f68751bea
Yerkebulan Kussym <e_kusym@kbtu.kz>
2023-04-06 01:56:36 +0600

    updd

--- LAB_8/api/urls.py
+++ LAB_8/api/urls.py
@@ -1,21 +1,15 @@
-"""api URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.urls import path, include, re_path
+from api import views
 from django.contrib import admin
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
+    path('products/', views.product_list),
+    path('products/<int:product_id>/', views.product_detail),
+    path('categories/', views.cat_list),
+    path('categories/<int:id>/', views.cat_detail),
+    path('categories/<int:id>/products/', views.products_by_cat),
 ]
+
