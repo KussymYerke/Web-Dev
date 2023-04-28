4ab68f6f4fb0fae603fd05be90a7ee9628e13bae
Yerkebulan <e_kusym@kbtu.kz>
2023-04-29 01:37:16 +0600

    register code's logic

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {ToastrService} from "ngx-toastr";
+import {AuthService} from "../service/auth.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+
+  constructor(private builder:FormBuilder, private toastr:ToastrService,
+              private service:AuthService, private router:Router ) {
+
+  }
+
+  registerForm = this.builder.group({
+    id:this.builder.control('',Validators.compose([Validators.required, Validators.minLength(5)]) ),
+    name:this.builder.control('',Validators.required),
+    password:this.builder.control('',Validators.required),
+    email:this.builder.control('',Validators.compose([Validators.required, Validators.email])),
+    gender:this.builder.control('male'),
+    role:this.builder.control(''),
+    isactive:this.builder.control(false)
+  });
+
+  proceedregistration(){
+    if(this.registerForm.valid){
+      this.service.Proceedregister(this.registerForm.value).subscribe(res=>{
+        this.toastr.success('User registered successfully');
+        this.router.navigate(['login']);
+      });
+    }else{
+      this.toastr.warning('Please send a valid data');
+    }
+  }
+}
